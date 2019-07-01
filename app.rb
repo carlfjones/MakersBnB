c9df9433097e5b9d73ad2ee373894aeb24313336
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-07-01 17:32:01 +0100

    Sign up page

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
 require 'sinatra/base'
 
 class Makersbnb < Sinatra::Base
+  enable :sessions
+
   get '/' do
     'Hello World'
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  get '/home' do
+    @username = session[:user]
+    erb :home
+  end
+
+  post '/signup/new' do
+    session[:user] = params[:username]
+    redirect "/home"
   end
 
   run! if app_file == $0
