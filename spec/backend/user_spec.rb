1e5b5590be7b5510270ea1020ec43de359dfd992
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-07-01 17:09:12 +0100

    First unit tests for user class PASS

---
+++ spec/backend/user_spec.rb
@@ -0,0 +1,13 @@
+require 'user.rb'
+
+describe User do
+  describe '.create' do
+    it 'creates a new user' do
+      user = User.create(email: 'test@email.com', password: 'pass123')
+
+      expect(user).to be_a User
+      expect(user.id).not_to be_empty
+      expect(user.email).to eq 'test@email.com'
+    end
+  end
+end
