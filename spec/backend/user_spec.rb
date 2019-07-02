79b36b9cefc67c4a7fb2cb4765be186f3decad70
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-07-02 14:08:10 +0100

    Unit test for Space

--- spec/backend/user_spec.rb
+++ spec/backend/user_spec.rb
@@ -6,9 +6,21 @@ describe User do
       user = User.create(username: 'Boss', email: 'test@email.com', password: 'pass123')
 
       expect(user).to be_a User
-      expect(user.username).to eq 'Boss'
+      expect(user.username).to eq('Boss')
       expect(user.id).not_to be_empty
-      expect(user.email).to eq 'test@email.com'
+      expect(user.email).to eq('test@email.com')
+    end
+  end
+
+  describe '.find' do
+    it 'finds user by id' do
+      user = User.create(username: 'Boss', email: 'test@email.com', password: 'pass123')
+      test_id = user.id
+      found_user = User.find(id: test_id)
+      expect(found_user).to be_a User
+      expect(found_user.username).to eq('Boss')
+      expect(found_user.id).to eq(test_id)
+      expect(found_user.email).to eq('test@email.com')
     end
   end
 end
